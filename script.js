550d6499a0655e281a7db2401e48fb83b8b849cf
Kiniun <phreshpresh@pop-os.localdomain>
2023-01-13 12:06:54 +0100

    marker popup added

--- script.js
+++ script.js
@@ -10,3 +10,40 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+        const {latitude} = position.coords;
+        const {longitude} = position.coords
+        const coords = [latitude, longitude]
+        const map = L.map('map').setView(coords, 13);
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        // L.marker(coords).addTo(map)
+        //     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        //     .openPopup();
+
+        map.on('click', function (mapEvent) {
+
+            form.classList.remove('hidden')
+            inputDistance.focus()
+        //     console.log(mapEvent);
+        //     const {lat, lng} = mapEvent.latlng
+        //
+        //
+        // L.marker([lat, lng]).addTo(map)
+        //     .bindPopup(L.popup({
+        //         maxWidth: 250,
+        //         minWidth: 100,
+        //         autoClose: false,
+        //         closeOnClick: false,
+        //         className: 'running-popup'
+        //     })).setPopupContent('Workout')
+        //     .openPopup();
+        })
+    }, function () {
+        alert('Could not get your position')
+    })
+}
